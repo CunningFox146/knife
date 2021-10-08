4296f1dd0b68e99d431da78692694ca354bdbdf1
Makar <makar.papca@gmail.com>
2021-10-08 21:55:58 +0300

    Added views
    Added some basic ui elements

---
+++ Assets/Scripts/UI/Guide.cs
@@ -0,0 +1,46 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KnifeGame.UI
+{
+    public class Guide : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _endPos;
+        [SerializeField] private Vector3 _endRot;
+
+        private Sequence _sequence;
+
+        private void Start()
+        {
+            var clearColor = new Color(1, 1, 1, 0);
+            var image = GetComponent<Image>();
+            var rectTrans = transform as RectTransform;
+            var startRot = rectTrans.eulerAngles;
+
+            image.color = clearColor;
+
+            _sequence = DOTween.Sequence()
+                .SetDelay(1f)
+                .SetLoops(-1)
+                /*TIMELINE*/
+
+                .Append(rectTrans.DORotate(_endRot, 0.35f).SetEase(Ease.OutCubic))
+                .Join(image.DOColor(Color.white, 0.35f).SetEase(Ease.OutCubic))
+
+                .Append(rectTrans.DOAnchorPos(_endPos, 1f).SetEase(Ease.InOutSine))
+
+                .Append(rectTrans.DORotate(startRot, 0.35f).SetEase(Ease.InCubic))
+                .Join(image.DOColor(clearColor, 0.35f).SetEase(Ease.InCubic));
+        }
+
+        private void OnDestroy()
+        {
+            if (_sequence != null)
+            {
+                _sequence.Kill();
+                _sequence = null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Views/MainView.cs
@@ -0,0 +1,41 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KnifeGame.UI.Views
+{
+    public class MainView : View
+    {
+        [SerializeField] private RectTransform _stats;
+        [SerializeField] private GameObject _guide;
+        [SerializeField] private Text _title;
+        [SerializeField] private Text _score;
+        [SerializeField] private Text _scoreMax;
+        [SerializeField] private Text _coins;
+
+        private Vector2 _statsPos;
+
+        private void Start()
+        {
+            ShowTitle();
+        }
+
+        private void ShowTitle()
+        {
+            var titleScale = _title.rectTransform.localScale;
+            _title.rectTransform.localScale = new Vector3(titleScale.x, 0f, titleScale.z);
+            _title.rectTransform.DOScale(titleScale, 0.5f).SetEase(Ease.OutBounce);
+
+            _statsPos = _stats.anchoredPosition;
+            _stats.anchoredPosition = _statsPos + Vector2.up * 300f;
+        }
+
+        private void HideTitle()
+        {
+            _stats.DOAnchorPos(_statsPos, 0.5f);
+            _title.rectTransform.DOScale(Vector3.zero, 0.5f).SetEase(Ease.InBounce);
+
+            Destroy(_guide);
+        }
+    }
+}
