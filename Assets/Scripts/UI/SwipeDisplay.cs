67c3497c6f5d2548febea1f94641863bd439df79
Makar <makar.papca@gmail.com>
2021-10-12 19:43:45 +0300

    Refactored SwipeDisplay

--- Assets/Scripts/UI/SwipeDisplay.cs
+++ Assets/Scripts/UI/SwipeDisplay.cs
@@ -7,11 +7,30 @@ namespace KnifeGame.UI
     public class SwipeDisplay : MonoBehaviour
     {
         [SerializeField] private Canvas _canvas;
-        [SerializeField] private GameObject _start;
-        [SerializeField] private List<GameObject> _dots;
-        [SerializeField] private GameObject _end;
+        [SerializeField] private GameObject _startPrefab;
+        [SerializeField] private GameObject _middlePrefab;
+        [SerializeField] private GameObject _endPrefab;
+        [SerializeField] private int _dotsCount = 5;
 
+        private RectTransform _start;
+        private RectTransform _end;
+        private List<RectTransform> _dots;
         private Vector3 _swipeStart;
+
+        private void Awake()
+        {
+            _dots = new List<RectTransform>();
+            for (int i = 0; i < _dotsCount; i++)
+            {
+                _dots.Add(
+                    Instantiate(_middlePrefab, transform).GetComponent<RectTransform>()
+                 );
+            }
+            _start = Instantiate(_startPrefab, transform).GetComponent<RectTransform>();
+            _end = Instantiate(_endPrefab, transform).GetComponent<RectTransform>();
+
+            Disable();
+        }
 
         private void Start()
         {
@@ -28,37 +47,39 @@ namespace KnifeGame.UI
 
         private void Enable()
         {
-            _start.SetActive(true);
-            _end.SetActive(true);
-            _dots.ForEach((item) => item.SetActive(true));
+            _start.gameObject.SetActive(true);
+            _end.gameObject.SetActive(true);
+            _dots.ForEach((item) => item.gameObject.SetActive(true));
         }
         private void Disable()
         {
-            _start.SetActive(false);
-            _end.SetActive(false);
-            _dots.ForEach((item) => item.SetActive(false));
+            _start.gameObject.SetActive(false);
+            _end.gameObject.SetActive(false);
+            _dots.ForEach((item) => item.gameObject.SetActive(false));
         }
 
         private void OnSwipeStartHandler(Vector2 start)
         {
             Enable();
-            OnSwipeDragHandler(start);
 
             _swipeStart = ConvertPos(start);
-            _start.transform.position = _swipeStart;
+            _start.position = _swipeStart;
+
+            OnSwipeDragHandler(start);
         }
 
         private void OnSwipeDragHandler(Vector2 pos)
         {
             var dragPos = ConvertPos(pos);
-            _end.transform.position = dragPos;
+            _end.position = dragPos;
 
             Vector3 dist = dragPos - _swipeStart;
-            for (int i = 0; i < _dots.Count; i++)
+            for (int i = 0; i < _dotsCount; i++)
             {
-                float percent = (float)i / (float)_dots.Count;
+                float percent = i / (float)_dotsCount;
                 var dot = _dots[i];
-                dot.transform.position = dist * percent + _swipeStart;
+                dot.position = dist * percent + _swipeStart;
+                dot.localScale = Vector3.one * (0.5f + percent * 0.5f);
             }
         }
     }
