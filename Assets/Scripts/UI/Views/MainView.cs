2823a8998a51a03bb897d5523b1642add6f1d345
Makar <makar.papca@gmail.com>
2021-10-13 23:21:50 +0300

    Added text mesh pro and started reworking logo

--- Assets/Scripts/UI/Views/MainView.cs
+++ Assets/Scripts/UI/Views/MainView.cs
@@ -1,5 +1,6 @@
 ﻿using DG.Tweening;
 using KnifeGame.Managers;
+using KnifeGame.Util;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,17 +15,16 @@ namespace KnifeGame.UI.Views
         [SerializeField] private RectTransform _stats;
         [SerializeField] private Guide _guide;
 
-        [SerializeField] private Text _title;
         [SerializeField] private Text _score;
         [SerializeField] private Text _scoreMax;
         [SerializeField] private Text _coins;
 
         private Vector2 _statsPos;
-        private bool _titleVisible = true;
+        private bool _statsHidden = true;
 
         private void Start()
         {
-            ShowTitle();
+            HideStats();
 
             SwipeManager.Inst.OnSwipeStart += OnSwipeStartHandler;
             ScoreManager.Inst.OnKnifeFlip += OnKnifeFlipHandler;
@@ -43,33 +43,24 @@ namespace KnifeGame.UI.Views
 
         private void OnSwipeStartHandler(Vector2 start)
         {
-            if (_titleVisible)
+            if (_statsHidden)
             {
-                _titleVisible = false;
-                HideTitle();
+                _statsHidden = false;
+                ShowStats();
             }
         }
 
-        private void ShowTitle()
+        private void HideStats()
         {
-            var titleScale = _title.rectTransform.localScale;
-            _title.rectTransform.localScale = new Vector3(titleScale.x, 0f, titleScale.z);
-            _title.rectTransform.DOScale(titleScale, 1f).SetEase(Ease.OutBounce);
-
             _statsPos = _stats.anchoredPosition;
             _stats.anchoredPosition = _statsPos + Vector2.up * 300f;
             _stats.gameObject.SetActive(false);
         }
 
-        private void HideTitle()
+        private void ShowStats()
         {
             _stats.gameObject.SetActive(true);
             _stats.DOAnchorPos(_statsPos, 0.5f).SetEase(Ease.OutBack);
-
-            var titleScale = _title.rectTransform.localScale;
-            _title.rectTransform.DOScale(new Vector3(titleScale.x, 0f, titleScale.z), 0.5f)
-                .SetEase(Ease.InBack)
-                .OnComplete(() => _title.gameObject.SetActive(false));
 
             _guide.Kill();
         }
