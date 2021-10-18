ab22714e2236544925e9fe432e0b7829fd8e9547
Makar <makar.papca@gmail.com>
2021-10-18 21:44:54 +0300

    Added shop sprites

---
+++ Assets/_Core/Scripts/UI/Views/ShopView.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using UnityEngine;
+
+namespace KnifeGame.UI.Views
+{
+    public class ShopView : View
+    {
+        private CanvasGroup _canvasGroup;
+        private RectTransform _rectTrans;
+
+        private Sequence _animSequence;
+
+        private void Awake()
+        {
+            _canvasGroup = GetComponent<CanvasGroup>();
+            _rectTrans = (RectTransform)transform;
+        }
+
+        private void Start()
+        {
+            Show();
+        }
+
+        public override void Show()
+        {
+            base.Show();
+
+            KillSequence();
+
+            float duration = 0.25f;
+            _canvasGroup.alpha = 0f;
+            _rectTrans.anchoredPosition = Vector2.up * 50f;
+
+            _animSequence = DOTween.Sequence()
+                .Append(_canvasGroup.DOFade(1f, duration))
+                .Join(_rectTrans.DOAnchorPos(Vector2.zero, duration))
+                .SetEase(Ease.OutCubic);
+        }
+
+        private void KillSequence()
+        {
+            if (_animSequence != null)
+            {
+                _animSequence.Kill();
+                _animSequence = null;
+            }
+        }
+    }
+}
