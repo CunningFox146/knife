3fad667163227604c2eae52f5c6214ae693b14dc
Makar <makar.papca@gmail.com>
2021-10-19 17:59:56 +0300

    Added animated view

---
+++ Assets/_Core/Scripts/UI/Views/AnimatedView.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using KnifeGame.Managers;
+using UnityEngine;
+
+namespace KnifeGame.UI.Views
+{
+    public class AnimatedView : View
+    {
+        protected CanvasGroup _canvasGroup;
+        protected RectTransform _rectTrans;
+
+        protected Sequence _animSequence;
+
+        protected virtual void Awake()
+        {
+            _canvasGroup = GetComponent<CanvasGroup>();
+            _rectTrans = (RectTransform)transform;
+        }
+
+        public override void Show()
+        {
+            base.Show();
+
+            KillSequence();
+
+            float duration = 0.25f;
+            _canvasGroup.alpha = 0f;
+            _rectTrans.anchoredPosition = Vector2.up * 50f;
+
+            _animSequence = DOTween.Sequence()
+                .Append(_canvasGroup.DOFade(1f, duration))
+                .Join(_rectTrans.DOAnchorPos(Vector2.zero, duration))
+                .SetEase(Ease.OutCubic);
+        }
+
+        public override void Hide()
+        {
+            KillSequence();
+
+            float duration = 0.25f;
+
+            _animSequence = DOTween.Sequence()
+                .Append(_canvasGroup.DOFade(0f, duration))
+                .Join(_rectTrans.DOAnchorPos(Vector2.up * 50f, duration))
+                .SetEase(Ease.OutCubic)
+                .OnComplete(()=>base.Hide());
+        }
+
+        protected void KillSequence()
+        {
+            if (_animSequence != null)
+            {
+                _animSequence.Kill();
+                _animSequence = null;
+            }
+        }
+    }
+}
--- Assets/_Core/Scripts/UI/Views/ShopView.cs
+++ Assets/_Core/Scripts/UI/Views/ShopView.cs
@@ -7,60 +7,8 @@ using UnityEngine;
 
 namespace KnifeGame.UI.Views
 {
-    public class ShopView : View
+    public class ShopView : AnimatedView
     {
-        private CanvasGroup _canvasGroup;
-        private RectTransform _rectTrans;
-
-        private Sequence _animSequence;
-
-        private void Awake()
-        {
-            _canvasGroup = GetComponent<CanvasGroup>();
-            _rectTrans = (RectTransform)transform;
-        }
-
-        private void Start()
-        {
-            Show();
-        }
-
-        public override void Show()
-        {
-            base.Show();
-
-            KillSequence();
-
-            float duration = 0.25f;
-            _canvasGroup.alpha = 0f;
-            _rectTrans.anchoredPosition = Vector2.up * 50f;
-
-            _animSequence = DOTween.Sequence()
-                .Append(_canvasGroup.DOFade(1f, duration))
-                .Join(_rectTrans.DOAnchorPos(Vector2.zero, duration))
-                .SetEase(Ease.OutCubic);
-        }
-
-        public override void Hide()
-        {
-            KillSequence();
-
-            float duration = 0.25f;
-
-            _animSequence = DOTween.Sequence()
-                .Append(_canvasGroup.DOFade(0f, duration))
-                .Join(_rectTrans.DOAnchorPos(Vector2.up * 50f, duration))
-                .SetEase(Ease.OutCubic)
-                .OnComplete(()=>base.Hide());
-        }
-
-        private void KillSequence()
-        {
-            if (_animSequence != null)
-            {
-                _animSequence.Kill();
-                _animSequence = null;
-            }
-        }
+        
     }
 }
