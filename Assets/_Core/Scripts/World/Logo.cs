c22014aae1db510c15eea2ba8244080283b9031b
Makar <makar.papca@gmail.com>
2021-10-20 14:32:45 +0300

    Fixed logo dotween warning

--- Assets/_Core/Scripts/World/Logo.cs
+++ Assets/_Core/Scripts/World/Logo.cs
@@ -8,18 +8,23 @@ namespace KnifeGame.World
 {
     public class Logo : MonoBehaviour
     {
+        private Tween _showTween;
+
         private void Start()
         {
             SwipeManager.Inst.OnSwipeStart += OnSwipeStartHandler;
 
             var pos = transform.position;
             transform.position = pos + Vector3.up * 4f;
-            transform.DOMove(pos, 2f).SetEase(Ease.OutElastic);
+            _showTween = transform.DOMove(pos, 2f).SetEase(Ease.OutElastic);
         }
 
         private void OnSwipeStartHandler(Vector2 obj)
         {
             SwipeManager.Inst.OnSwipeStart -= OnSwipeStartHandler;
+
+            _showTween?.Kill();
+
             DOTween.Sequence()
             .Append(transform.DOMove(Vector3.down, 0.25f)
                 .SetEase(Ease.Linear)
