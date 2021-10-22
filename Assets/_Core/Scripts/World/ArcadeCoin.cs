7cda9a9b5cac47bb88b3cbfbad173e6ae3c3288a
Makar <makar.papca@gmail.com>
2021-10-22 20:50:25 +0300

    Added arcade coin

---
+++ Assets/_Core/Scripts/World/ArcadeCoin.cs
@@ -0,0 +1,32 @@
+﻿using DG.Tweening;
+using KnifeGame.Managers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KnifeGame.World
+{
+    public class ArcadeCoin: MonoBehaviour
+    {
+        private Rigidbody _rb;
+        private Tween _removeTween;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            StatsManager.Inst.CoinsCount++;
+
+            _rb.constraints = RigidbodyConstraints.None;
+
+            _removeTween = transform.DOScale(Vector3.zero, 1f)
+                .SetEase(Ease.InBack)
+                .OnComplete(()=>Destroy(gameObject));
+        }
+    }
+}
