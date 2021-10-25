bb8f25cfb559f9aee64214d18ff789e1ff30a01e
Makar <makar.papca@gmail.com>
2021-10-25 16:41:06 +0300

    Added level part for arcade game mode

---
+++ Assets/_Core/Scripts/World/Arcade/ArcadeWorldGeneration.cs
@@ -0,0 +1,35 @@
+﻿using KnifeGame.Knife;
+using KnifeGame.Managers;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KnifeGame.World
+{
+    public class ArcadeWorldGeneration : MonoBehaviour
+    {
+        [SerializeField] private GameObject _coinPrefab;
+        [SerializeField] private GameObject[] _branches;
+        [Space]
+        [SerializeField] private float _partSize = 10f;
+        [SerializeField] private float _minX = -5f;
+        [SerializeField] private float _maxX = 5f;
+
+        private float _lastY = 0f;
+
+        private List<Transform> _items;
+
+        private void Awake()
+        {
+            _items = new List<Transform>();
+        }
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+            
+        }
+    }
+}
